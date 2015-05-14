bcd741f4225f5cd082a6f91981f2013984e34215
Stefan Hendriks <stefanhen83@gmail.com>
2015-05-14 16:05:38 +0200

    remove dead code

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -1,15 +1,6 @@
 class WizardController < ApplicationController
 
   def index
-
-  end
-
-  def developer
-    @role = Developer.new
-  end
-
-  def organisation
-    @role = Organisation.new
   end
 
 end
