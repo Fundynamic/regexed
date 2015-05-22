40ca5a2acc88910aaeffb9509b974db9d0f5f6ca
Stefan Hendriks <stefanhen83@gmail.com>
2015-05-22 16:26:39 +0200

    use database cleaner to clean up state for each test run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,27 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-# The settings below are suggested to provide a good initial experience
+  config.before(:all) do
+    # start with cleaning before running any specs,
+    # since the migrations may have inserted some data which we don't want in our specs
+    DatabaseCleaner.clean_with :deletion
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+    # Timecop.return
+  end
+
+  # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
   # These two settings work together to allow you to limit a spec run
