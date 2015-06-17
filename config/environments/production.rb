4680c4640bbffaf9ef42c3ddc4365e49561e71a7
Stefan Hendriks <stefanhen83@gmail.com>
2015-06-17 20:55:24 +0200

    precompile fonts in production explicitly and refer to fonts different

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Regexed::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
